cb3b1e2bda7e0f72338338ba574ead9e650a3e38
ArnavTubeHD <94029446+ArnavTubeHD@users.noreply.github.com>
2021-11-12 14:15:17 +0530

    Update main.py

--- watch time bot/main.py
+++ watch time bot/main.py
@@ -1,9 +1,9 @@
 import time
 from selenium import webdriver
 
-video_link = 'https://www.youtube.com/watch?v=uloK1HOTL1c'
-views = 100
-video_duration = 7 * 60
+video_link = 'https://www.youtube.com/watch?v=uK1WY0ztMVE'
+views = 1000
+video_duration = 18 * 30
 
 driver = webdriver.Chrome()
 driver.get(video_link)
